3d74c06c9c2661bcfc9c871b63a24e04494c3db7
pask517 <andreapascarella99@gmail.com>
2026-02-06 16:31:31 +0100

    added spring logo

---
+++ src/main/java/andreapascarella/gestione_prenotazioni/runners/CleanRunner.java
@@ -0,0 +1,18 @@
+package andreapascarella.gestione_prenotazioni.runners;
+
+import org.springframework.boot.CommandLineRunner;
+
+public class CleanRunner implements CommandLineRunner {
+    @Override
+    public void run(String... args) throws Exception {
+
+        //  .   ____          _            __ _ _
+        // /\\ / ___'_ __ _ _(_)_ __  __ _ \ \ \ \
+        //( ( )\___ | '_ | '_| | '_ \/ _` | \ \ \ \
+        // \\/  ___)| |_)| | | | | || (_| |  ) ) ) )
+        //  '  |____| .__|_| |_|_| |_\__, | / / / /
+        // =========|_|==============|___/=/_/_/_/
+
+
+    }
+}
